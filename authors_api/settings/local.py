758045004332cc6f031134c1fd29328d9f5b863f
towhid7667 <towhidulhq@gmail.com>
2024-01-31 23:51:37 +0600

    first commit

---
+++ authors_api/settings/local.py
@@ -0,0 +1,12 @@
+from .base import * # noqa
+from .base import env
+
+
+
+SECRET_KEY = env("DJANGO_SECRET_KEY", default = "Qn_jPjlguugAmX-eifchMO2-cfemzToJhk97j13UG7KxepDUqmc")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
---
+++ authors_api/settings/production.py
@@ -0,0 +1,6 @@
+from .base import * # noqa
+from .base import env
+
+ADMINS = [("Towhid", "towhidulhq@gmail.com")]
+
+CSRF_TRUSTED_ORIGINS = []
